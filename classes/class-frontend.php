a506b28eee4f127da12e06511a5dd27b5765d16f
Javier Villanueva <javiervd@gmail.com>
2014-02-02 20:27:44 -0430

    Add frontend emoji rendering functionality

--- classes/class-frontend.php
+++ classes/class-frontend.php
@@ -7,7 +7,7 @@
  */
 
 //avoid direct calls to this file
-if ( ! function_exists( 'add_filter' ) ) {
+if ( ! function_exists( 'ABSPATH' ) ) {
 	header( 'Status: 403 Forbidden' );
 	header( 'HTTP/1.1 403 Forbidden' );
 	exit();
@@ -15,14 +15,14 @@ if ( ! function_exists( 'add_filter' ) ) {
 
 /**
  * @todo Description
- * 
+ *
  * @since	1.0.0
  */
 class WPCollab_HelloEmoji_Frontend {
-	
+
 	/**
 	 * Holds a copy of the object for easy reference.
-	 * 
+	 *
 	 * @since	1.0.0
 	 * @static
 	 * @access	private
@@ -32,11 +32,11 @@ class WPCollab_HelloEmoji_Frontend {
 
 	/**
 	 * Getter method for retrieving the object instance.
-	 * 
+	 *
 	 * @since	1.0.0
 	 * @static
 	 * @access	public
-	 * 
+	 *
 	 * @return	object	WPCollab_HelloEmoji_Frontend::$instance
 	 */
 	public static function get_instance() {
@@ -47,24 +47,26 @@ class WPCollab_HelloEmoji_Frontend {
 
 	/**
 	 * Constructor. Hooks all interactions to initialize the class.
-	 * 
+	 *
 	 * @since	1.0.0
 	 * @access	public
-	 * 
+	 *
 	 * @return	void
 	 */
 	public function __construct() {
-		
+
 		self::$instance = $this;
-		
 
 		// Filter the_content to add a css class to the content
 		add_filter( 'the_content', array( $this, 'wrap_content' ) );
 		// Filter the_content to add a css class to the comment
 		add_filter( 'comment_text', array( $this, 'wrap_content' ) );
 
+		// Load admin JavaScript
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_frontend_scripts' ) );
+
 	} // END __construct()
-		
+
 	/**
 	 * Add class to content and comment
 	 *
@@ -87,6 +89,23 @@ class WPCollab_HelloEmoji_Frontend {
 		}
 
 		return $content;
+
 	} // END wrap_content()
+
+	/**
+	 * Enqueue frontend scripts
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 *
+	 * @return  void
+	 */
+	public function enqueue_frontend_scripts() {
+
+		wp_enqueue_script( 'jquery-emoji-script', plugins_url( 'lib/jquery-emoji/jquery.emoji.js', WPCollab_HelloEmoji::get_file() ), array( 'jquery' ), WPCollab_HelloEmoji::$version, true );
+
+		wp_enqueue_script( 'hello-emoji-script', plugins_url( 'js/hello-emoji.js', WPCollab_HelloEmoji::get_file() ), array( 'jquery-emoji-script' ), WPCollab_HelloEmoji::$version, true );
+
+	} // END enqueue_frontend_scripts()
 
 } // END class WPCollab_HelloEmoji_Frontend
--- js/hello-emoji.js
+++ js/hello-emoji.js
@@ -1,6 +1,6 @@
 /**
- * @todo short description
- * 
+ * Emoji frontend rendering functionality
+ *
  * @author		WPCollab Team
  * @copyright	Copyright (c) 2014, WPCollab Team
  * @license		http://www.gnu.org/licenses/gpl-2.0.html GPLv2
@@ -8,5 +8,14 @@
  * @version		0.1-alpha
  */
 
+(function ( $ ) {
 
+	'use strict';
 
+	$(function () {
+
+		$('.wpcollab-hello-emoji').emoji();
+
+	});
+
+}(jQuery));
