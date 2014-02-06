7b7f1f21b908f60d6df8d9839877e63453a0a97e
Yoren Chang <yorenchang@gmail.com>
2014-02-06 19:30:22 +0800

    Remove codes for Emoji Settings. See #29

--- classes/class-admin.php
+++ classes/class-admin.php
@@ -58,7 +58,6 @@ class WPCollab_HelloEmoji_Admin {
 		self::$instance = $this;
 
 		add_action( 'admin_init', array( $this, 'register_settings' ) );
-		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
 		/** Load admin JavaScript. */
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
 
@@ -120,89 +119,7 @@ class WPCollab_HelloEmoji_Admin {
 			)
 		);
 
-		add_settings_section(
-			'post_types', // Section name
-			__( 'Post Types', 'hello-emoji' ), // Section title
-			array( $this, 'post_types_desc'), // Callback function
-			'wpcollab_hello_emoji_settings' // Page on which to display
-		);
-
-		/**Create an option to enable emojis per post type**/
-		//get all register post types
-		$post_types = get_post_types();
-		//remove attachments, revisions and nav_menu_items from array
-		unset($post_types["attachment"], $post_types["revision"], $post_types["nav_menu_item"] );
-		foreach ($post_types as $post_type ) {
-			/**Prepare to make setting**/
-			//get post type object
-			$obj = get_post_type_object( $post_type );
-			//get the label for the post type for display use
-			$post_type_label = $obj->labels->name;
-			//create option name with post type name
-			$uniqueOptionName = $post_type;
-
-			//This will have to loop, make a create settings field function and pass the field ID
-			add_settings_field(
-				$uniqueOptionName, // ID
-				__( 'Enable for ' . $post_type_label, 'hello-emoji' ), // Label
-				array( $this, 'per_post_types' ), // Callback
-				'wpcollab_hello_emoji_settings', // Page on which to display
-				'post_types', // Section
-				array(
-					$uniqueOptionName
-				)
-			);
-		}
-
-		add_settings_section(
-			'comments', // Section name
-			__( 'Comments', 'hello-emoji' ), // Section title
-			array( $this, 'comments_desc'), // Callback function
-			'wpcollab_hello_emoji_settings' // Page on which to display
-		);
-
-		add_settings_field(
-				'comment', // ID
-				__( 'Enable for Comments', 'hello-emoji' ), // Label
-				array( $this, 'comments' ), // Callback
-				'wpcollab_hello_emoji_settings', // Page on which to display
-				'comments', // Section
-				array(
-					'label_for' => 'wpcollab_hello_emoji_comment'
-				)
-			);
-
-		register_setting(
-			'wpcollab_hello_emoji_settings',
-			'wpcollab_hello_emoji_settings'
-		);
-
 	} // END register_settings()
-
-	/**
-	 * @todo description
-	 *
-	 * @since	1.0.0
-	 * @access	public
-	 *
-	 * @see	add_submenu_page()
-	 * @see	apply_filters()
-	 *
-	 * @return	void
-	 */
-	public function admin_menu() {
-
-		$this->pagehook = add_options_page(
-			__( 'Emoji Settings', 'hello-emoji' ),
-			__( 'Emoji', 'hello-emoji' ),
-			apply_filters( 'wpcollab_hello_emoji_settings_cap', 'manage_options' ),
-			'emoji',
-			array( $this, 'settings_page' )
-		);
-
-		add_action( "load-{$this->pagehook}", array( $this, 'help_tabs' ) );
-
-	} // END admin_menu()
 
 	/**
 	 * @todo
@@ -228,20 +145,6 @@ class WPCollab_HelloEmoji_Admin {
 	} // END help_tabs()
 
 	/**
-	* Callbacks for post type enable options
-	*
-	* @since 0.1.0
-	*/
-	function per_post_types( $args ) {
-		$settings = get_option( 'wpcollab_hello_emoji_settings' );
-		$setting = ( isset( $settings[$args[0]] ) ) ? esc_attr( $settings[$args[0]] ) : false;
-		$post_type = $args[0];
-		$checked = checked( '1', $setting, false );
-
-		echo "<input type='checkbox' id='wpcollab_hello_emoji_$post_type' name='wpcollab_hello_emoji_settings[$post_type]' value='1' $checked />";
-	}
-
-	/**
 	 * @todo
 	 *
 	 * @since 1.0
@@ -252,27 +155,8 @@ class WPCollab_HelloEmoji_Admin {
 		$setting = ( isset( $settings['comment'] ) ) ? esc_attr( $settings['comment'] ) : false;
 		$checked = checked( '1', $setting, false );
 
-		echo "<p><span><em>Hello Emoji enable emoji in post content by default.</em></span><br />";
-		echo "<input type='checkbox' id='wpcollab_hello_emoji_comment' name='wpcollab_hello_emoji_settings[comment]' value='1' $checked /> <span>Enable emoji in comments, too.</span></p>";
-	}
-
-	/**
-	 * @todo
-	 *
-	 * @since 1.0
-	 */
-	function post_types_desc() {
-		echo '<p>' . __( 'Check the post types you\'d like to enable Emoji.', 'hello-emoji' ) . '</p>';
-	}
-
-	/**
-	 * @todo
-	 *
-	 * @since 1.0
-	 */
-	function comments_desc() {
-
-		echo '<p>' . __( 'Check if you want to enable Emoji for the comments.', 'hello-emoji' ) . '</p>';
+		echo "<input type='checkbox' id='wpcollab_hello_emoji_comment' name='wpcollab_hello_emoji_settings[comment]' value='1' $checked /> <span>Enable emoji in comments, too.</span>";
+		echo '<p class="description">' . __( 'Hello Emoji enable emoji in post content by default.', 'hello-emoji' ) . '</p>';
 	}
 
 	/**
